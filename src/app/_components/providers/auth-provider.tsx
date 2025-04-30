03a0ae622c99c8d0814ab0c2eb93814a87c5c502
Mike Purvis <mikedotexe@gmail.com>
2025-04-29 23:45:51 -0700

    optimization for navbar and other components, given our db sessions

--- src/app/_components/providers/auth-provider.tsx
+++ src/app/_components/providers/auth-provider.tsx
@@ -4,7 +4,11 @@ import { SessionProvider } from 'next-auth/react';
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider basePath="/api/auth">
+    <SessionProvider 
+      basePath="/api/auth"
+      refetchInterval={0}
+      refetchOnWindowFocus={false}
+    >
       {children}
     </SessionProvider>
   );
